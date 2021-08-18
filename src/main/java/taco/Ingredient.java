8f956396d717559891720c0f86f1e0d778f2b198
Traveller-hongchen <hongchenduzhe00@163.com>
2021-08-18 14:35:45 +0800

    安装了lombok之后进行了修改

--- src/main/java/taco/Ingredient.java
+++ src/main/java/taco/Ingredient.java
@@ -3,7 +3,6 @@ package taco;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 
-
 @Data
 @RequiredArgsConstructor
 public class Ingredient {
@@ -11,12 +10,6 @@ public class Ingredient {
     private final String id;
     private final String name;
     private final Type type;
-
-    public Ingredient(String id, String name, Type type) {
-        this.id = id;
-        this.name = name;
-        this.type = type;
-    }
 
     public static enum Type{
         WRAP, PROTEIN, VEGGIES, CHEESE, SAUCE
